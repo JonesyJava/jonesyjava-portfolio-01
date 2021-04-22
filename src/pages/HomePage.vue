f1df392b8dee06329ee0517fe14a8bb097d46558
Jonesy <jonesyjava@gmail.com>
2021-04-22 16:07:44 -0600

    added hover on square one

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="home container-fluid bg-black">
     <div class="row text-right">
-      <div class="col-6 pg-location">
-        <h1 class="font-large font-jj col-location text-white">JONESY</h1>
+      <div
+        class="col-6 pg-location"
+        data-aos="fade-left"
+        data-aos-offset="500"
+        data-aos-duration="1000"
+      >
+        <h1 class="font-large font-jj col-location text-white">Mike</h1>
         <br />
-        <h1 class="font-large font-jj text-white">JAVA</h1>
+        <h1 class="font-large font-jj text-white">Jones</h1>
       </div>
     </div>
     <div class="col-6">
       <div class="row">
-        <div class="square one-bg one-location blocksRowOne"></div>
+        <div
+          class="square one-bg one-location blocksRowOne hover-about text-center pt-4"
+        >
+          <h2 class="bio mt-3">BIO</h2>
+        </div>
       </div>
       <div class="row">
         <div class="square one-bg two-location blocksRowOne"></div>
@@ -33,8 +42,8 @@
 <script>
 import { onMounted } from 'vue'
 import anime from 'animejs/lib/anime.min.js'
-
 import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 AOS.init()
 export default {
@@ -45,22 +54,22 @@ export default {
       anime({
         targets: '.blocksRowOne',
         translateX: 0,
-        delay: anime.stagger(100, { start: 100 })
+        delay: anime.stagger(100, { start: 1000 })
       })
       anime({
         targets: '.blocksRowTwo',
         translateX: 150,
-        delay: anime.stagger(200, { start: 100 })
+        delay: anime.stagger(500, { start: 1000 })
       })
       anime({
         targets: '.blocksRowThree',
         translateX: 300,
-        delay: anime.stagger(200, { start: 100 })
+        delay: anime.stagger(500, { start: 1000 })
       })
       anime({
         targets: '.blocksRowFour',
         translateX: 450,
-        delay: anime.stagger(200, { start: 100 })
+        delay: anime.stagger(1000, { start: 1000 })
       })
     })
     return {
@@ -71,12 +80,31 @@ export default {
 </script>
 
 <style scoped lang="scss">
+// Hover within Blocks
+.hover-about:hover {
+  background-color: aliceblue;
+  cursor: pointer;
+}
+.hover-about:hover .bio {
+  display: inline-block;
+}
+.bio {
+  display: none;
+}
+.hover-about {
+  transition: all 0.25s;
+}
+// Background Color
 .bg-black {
   background-color: #000000;
 }
+
+// Font Size
 .font-large {
   font-size: 300px;
 }
+
+// Page Position
 .pg-location {
   position: absolute;
   top: 150px;
